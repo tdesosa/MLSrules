527ea6f94a8e978d5c643fd454fce97c03227522
tdesosa <tdesosa@gmail.com>
2019-02-01 16:39:57 -0700

    react example using create-react-app

--- mlsapp/src/Ninjas.js
+++ mlsapp/src/Ninjas.js
@@ -1,16 +1,29 @@
 import React from 'react';
 
-const Ninjas = (props) => {
-    const { ninjas } = props;
+const Ninjas = ({ ninjas, deleteNinja }) => {
     const ninjaList = ninjas.map(ninja => {
+        if (ninja.age > 20) {
         return (
             <div className="ninja" key={ninja.id}>
                 <div>Name: { ninja.name }</div>
                 <div>Age: { ninja.age }</div>
                 <div>Belt: { ninja.belt }</div>
+                <button onClick={() => {deleteNinja(ninja.id)}}>Delete Ninja</button>
             </div>
         )
+        } else {
+            return null;
+        }
     })
+    // const ninjaList = ninjas.map(ninja => {
+    //    return ninja.age > 20 ? (
+    //        <div className="ninja" key={ninja.id}>
+    //              <div>Name: { ninja.name }</div>
+    //              <div>Age: { ninja.age }</div>
+    //             <div>Belt: { ninja.belt }</div>
+    //         </div>
+    //    ) : null
+    // })
     return(
         <div className="ninja-list">
             { ninjaList }
